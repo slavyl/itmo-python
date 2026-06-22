8b55619d51dfc0a378528450f2ae2399a69e82e0
slavyl <twistem33@ya.ru>
2026-06-23 00:18:37 +0300

    выполнено задание 4.1 из p2

---
+++ p2/4.1.py
@@ -0,0 +1,10 @@
+celsius = float(input("Введите показания температуры в градусах Цельсия\n"))
+fahrenheit = celsius * (9 / 5) + 32
+
+print("В градусах по Фаренгейту:", fahrenheit)
+
+fahrenheit = float(input("Введите показания температуры в градусах по Фаренгейту\n"))
+celsius = (fahrenheit - 32) * (5 / 9)
+
+print("В градусах по Цельсию", celsius)
+
